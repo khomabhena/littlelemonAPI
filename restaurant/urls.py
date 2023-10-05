6a860eb6390623f8b94a9192ee4516d1fc8a25dc
khomabhena <colwanymab@gmail.com>
2023-10-05 16:59:10 +0200

    Added Menu Views and updated the urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu', views.MenuItemView.as_view(), name='index'),
+    path('menu', views.MenuItemView.as_view(), name='menu-item'),
+    path('menu/<int:id>', views.SingleMenuItemView.as_view(), name='single-item'),
 ]
