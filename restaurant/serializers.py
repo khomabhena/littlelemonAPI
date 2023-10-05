32f60ac22653a3d9415480df0e77e5f28e28d21d
khomabhena <colwanymab@gmail.com>
2023-10-05 16:44:45 +0200

    Added the User serialzer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -12,3 +12,8 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
