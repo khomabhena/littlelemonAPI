dcfdc92ad3bb9edad081c3f5030d0bb77f7fabd7
khomabhena <colwanymab@gmail.com>
2023-10-05 14:47:12 +0200

    Created models and made migrations

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+            
         }
     }
 }
